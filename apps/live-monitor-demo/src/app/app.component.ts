3c3e721b4ce564fe187fbe38e668a8a100c2bf69
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-08 12:36:28 +0200

    modify prettier config and run prettier

--- apps/live-monitor-demo/src/app/app.component.ts
+++ apps/live-monitor-demo/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'live-monitor-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'live-monitor-demo';
