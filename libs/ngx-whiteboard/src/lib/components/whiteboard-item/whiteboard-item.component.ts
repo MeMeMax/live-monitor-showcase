04a4cdc003e677b2618dc8182fb9b75335d94a24
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-08 15:25:11 +0200

    implement resizing of item

--- libs/ngx-whiteboard/src/lib/components/whiteboard-item/whiteboard-item.component.ts
+++ libs/ngx-whiteboard/src/lib/components/whiteboard-item/whiteboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'live-monitor-whiteboard-item',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./whiteboard-item.component.scss']
 })
 export class WhiteboardItemComponent implements OnInit {
+  @ViewChild('box') box!: ElementRef;
+
+  @Input() public width!: number;
+  @Input() public height!: number;
+
+  dragDisabled = false;
+
   constructor() {}
 
   ngOnInit(): void {}
--- libs/ngx-whiteboard/src/lib/directives/item-resize/item-resize.directive.ts
+++ libs/ngx-whiteboard/src/lib/directives/item-resize/item-resize.directive.ts
@@ -1,8 +1,59 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, HostListener, Input } from '@angular/core';
+
+import { WhiteboardItemComponent } from '@live-monitor/ngx-whiteboard';
 
 @Directive({
   selector: '[liveMonitorItemResize]'
 })
-export class ItemResizeDirective {
-  constructor() {}
+export class ItemResizeDirective implements AfterViewInit {
+  @Input() direction!: string;
+
+  boxPosition!: { left: number; top: number };
+  mouse!: { x: number; y: number };
+  grabbed = false;
+
+  constructor(private host: WhiteboardItemComponent) {}
+
+  ngAfterViewInit() {
+    console.log('afterViewInit');
+    this.loadBox();
+  }
+
+  private loadBox() {
+    const { left, top } = this.host.box.nativeElement.getBoundingClientRect();
+    this.boxPosition = { left, top };
+    console.log(this.boxPosition);
+  }
+
+  resize() {
+    console.log('resize');
+    console.log(this.direction);
+    if (this.direction === 'e' || this.direction === 'w') {
+      this.host.width = Number(this.mouse.x > this.boxPosition.left) ? this.mouse.x - this.boxPosition.left + 10 : 0;
+    }
+
+    if (this.direction === 'n' || this.direction === 's') {
+      this.host.height = Number(this.mouse.y > this.boxPosition.top) ? this.mouse.y - this.boxPosition.top + 10 : 0;
+    }
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.mouse = { x: event.clientX, y: event.clientY };
+    if (this.grabbed) {
+      this.resize();
+    }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.host.dragDisabled = true;
+    this.grabbed = true;
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    console.log('mouseup');
+    this.grabbed = false;
+  }
 }
