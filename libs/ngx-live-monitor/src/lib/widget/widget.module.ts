10936afe776f8d1aa972844b7aee4cd363a69db6
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 11:19:02 +0200

    add live-monitor and widget module to ngx-live-monitor

---
+++ libs/ngx-live-monitor/src/lib/widget/widget.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2022 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule]
+})
+export class WidgetModule {}
