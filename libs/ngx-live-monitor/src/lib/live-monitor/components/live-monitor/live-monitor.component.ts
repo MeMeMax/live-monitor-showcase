43f444ce16b442d4dab01ff6dbd2827ccb7ecfbe
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 11:57:16 +0200

    add whiteboard items by default

--- libs/ngx-live-monitor/src/lib/live-monitor/components/live-monitor/live-monitor.component.ts
+++ libs/ngx-live-monitor/src/lib/live-monitor/components/live-monitor/live-monitor.component.ts
@@ -17,7 +17,10 @@ export class LiveMonitorComponent implements OnInit {
     this.whiteboardItem$ = this.whiteboardStore.whiteboardItem$;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.whiteboardStore.addWhiteboardItem({ type: 'text', x: 0, y: 0, w: 6, h: 3 });
+    this.whiteboardStore.addWhiteboardItem({ type: 'text', x: 6, y: 0, w: 6, h: 3 });
+  }
 
   identifyItem(_index: number, item: WhiteboardItem) {
     return item.id;
