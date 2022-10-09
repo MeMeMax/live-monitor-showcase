3eeccd264b9eb5fd14f1c4a86eb97f7ab18a32fb
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 11:26:39 +0200

    provide whiteboard store service in live-monitor component

--- libs/ngx-live-monitor/src/lib/live-monitor/components/live-monitor/live-monitor.component.ts
+++ libs/ngx-live-monitor/src/lib/live-monitor/components/live-monitor/live-monitor.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WhiteboardStoreService } from '@live-monitor/ngx-whiteboard';
+
 @Component({
   selector: 'live-monitor-live-monitor',
   templateUrl: './live-monitor.component.html',
-  styleUrls: ['./live-monitor.component.scss']
+  styleUrls: ['./live-monitor.component.scss'],
+  providers: [WhiteboardStoreService]
 })
 export class LiveMonitorComponent implements OnInit {
   constructor() {}
--- libs/ngx-whiteboard/src/index.ts
+++ libs/ngx-whiteboard/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/ngx-whiteboard.module';
 
 export * from './lib/components/whiteboard/whiteboard.component';
 export * from './lib/components/whiteboard-item/whiteboard-item.component';
+
+export * from './lib/services/whiteboard-store/whiteboard-store.service';
