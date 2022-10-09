30c44cf7f348e0c18e70678b9da9cfccd16f3298
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 11:39:25 +0200

    import widget module in live-monitor module

--- libs/ngx-live-monitor/src/lib/live-monitor/live-monitor.module.ts
+++ libs/ngx-live-monitor/src/lib/live-monitor/live-monitor.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NgxWhiteboardModule } from '@live-monitor/ngx-whiteboard';
 
 import { LiveMonitorComponent } from './components/live-monitor/live-monitor.component';
+import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
   declarations: [LiveMonitorComponent],
-  imports: [CommonModule, NgxWhiteboardModule],
+  imports: [CommonModule, NgxWhiteboardModule, WidgetModule],
   exports: [LiveMonitorComponent]
 })
 export class LiveMonitorModule {}
