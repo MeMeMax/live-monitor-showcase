6d48658ca6e4803a00c148916cb3a6ca90877b2e
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 12:00:44 +0200

    create text-widget component

---
+++ libs/ngx-live-monitor/src/lib/widget/components/text/text-widget/text-widget.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2022 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'live-monitor-text-widget',
+  templateUrl: './text-widget.component.html',
+  styleUrls: ['./text-widget.component.scss']
+})
+export class TextWidgetComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/ngx-live-monitor/src/lib/widget/widget.module.ts
+++ libs/ngx-live-monitor/src/lib/widget/widget.module.ts
@@ -9,9 +9,10 @@ import { CommonModule } from '@angular/common';
 import { NgxWhiteboardModule } from '@live-monitor/ngx-whiteboard';
 
 import { DynamicWidgetDirective } from './directives/dynamic-widget/dynamic-widget.directive';
+import { TextWidgetComponent } from './components/text/text-widget/text-widget.component';
 
 @NgModule({
-  declarations: [DynamicWidgetDirective],
+  declarations: [DynamicWidgetDirective, TextWidgetComponent],
   imports: [CommonModule, NgxWhiteboardModule],
   exports: [DynamicWidgetDirective]
 })
