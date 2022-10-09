d783a6fceccea03a1ca7312b450389efc52d94bd
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 10:36:22 +0200

    add resize util

---
+++ libs/ngx-whiteboard/src/lib/utils/resize/resize.util.spec.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) 2022 Weidmueller Interface GmbH & Co. KG
+ * All rights reserved.
+ */
+
+import { Resize } from './resize.util';
+
+describe('Resize', () => {
+  it('should create an instance', () => {
+    expect(new Resize()).toBeTruthy();
+  });
+});
