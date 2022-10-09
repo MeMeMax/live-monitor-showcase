72045f8db023b2669cde02b20137e92fab960052
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2022-10-09 12:02:14 +0200

    register text-widget in widget registry

--- libs/ngx-live-monitor/src/lib/widget/widget.module.ts
+++ libs/ngx-live-monitor/src/lib/widget/widget.module.ts
@@ -10,10 +10,15 @@ import { NgxWhiteboardModule } from '@live-monitor/ngx-whiteboard';
 
 import { DynamicWidgetDirective } from './directives/dynamic-widget/dynamic-widget.directive';
 import { TextWidgetComponent } from './components/text/text-widget/text-widget.component';
+import { WidgetRegistryService } from './services/widget-registry/widget-registry.service';
 
 @NgModule({
   declarations: [DynamicWidgetDirective, TextWidgetComponent],
   imports: [CommonModule, NgxWhiteboardModule],
   exports: [DynamicWidgetDirective]
 })
-export class WidgetModule {}
+export class WidgetModule {
+  constructor(private widgetRegistry: WidgetRegistryService) {
+    this.widgetRegistry.registerWidget({ type: 'text', components: { widget: TextWidgetComponent }, size: { w: 100, h: 100 } });
+  }
+}
